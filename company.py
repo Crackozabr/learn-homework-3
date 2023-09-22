464cae39eba17aff6edde94728798304bcbece0a
Andrey Samokhin <samokhin.andrey.s@gmail.com>
2023-09-22 16:18:43 +0300

    second level done

--- company.py
+++ company.py
@@ -58,14 +58,15 @@ taxes = [
 ]
 
 # Ваши задачи такие:
-# 1. Вывести названия всех отделов
+
+print("\n1. Вывести названия всех отделов")
 print(*[department['title'] for department in departments], sep=', ')
-# 2. Вывести имена всех сотрудников компании.
+
+print("\n2. Вывести имена всех сотрудников компании")
 name_list = []
 for i, employers in enumerate([department['employers'] for department in departments]):
     name_list.extend([employer['first_name'] for employer in employers])
 print(*name_list, sep=', ')
-
 """
 # Вариант 2 для варианта 2
 name_list = []
@@ -75,26 +76,80 @@ for department in departments:
         #print(name_list)
 print(*name_list, sep=', ')
 """
-# 3. Вывести имена всех сотрудников компании с указанием отдела, в котором они работают.
+
+print("\n3. Вывести имена всех сотрудников компании с указанием отдела, в котором они работают")
 for department in departments:
     for employer in department['employers']:
         print(f"Department: {department['title']}; Employee: {employer['first_name']}")
 
-# 4. Вывести имена всех сотрудников компании, которые получают больше 100к.
+print("\n4. Вывести имена всех сотрудников компании, которые получают больше 100к")
 for department in departments:
     for employer in department['employers']:
         if employer['salary_rub'] > 100000:
             print(f"Employee {employer['first_name']} earns more than 100,000")
 
-# 5. Вывести позиции, на которых люди получают меньше 80к (можно с повторениями).
+print("\n5. Вывести позиции, на которых люди получают меньше 80к (можно с повторениями)")
 for department in departments:
     for employer in department['employers']:
         if employer['salary_rub'] < 100000:
             print(f"In department {department['title']}, employees earn less than 80,000")
             
-# 6. Посчитать, сколько денег в месяц уходит на каждый отдел – и вывести вместе с названием отдела
+print("\n6. Посчитать, сколько денег в месяц уходит на каждый отдел – и вывести вместе с названием отдела")
 for department in departments:
     department_salary = 0
     for employer in department['employers']:
         department_salary += employer['salary_rub']
     print(f"In department {department['title']}, employees earn a total of {department_salary}")
+
+# Второй уровень:
+print("\n7. Вывести названия отделов с указанием минимальной зарплаты в нём")
+for department in departments:
+    department_salary = []
+    for employer in department['employers']:
+        department_salary.append(employer['salary_rub'])
+    print(f"The minimum salary in department {department['title']} is {min(department_salary)}")
+
+print("\n8. Вывести названия отделов с указанием минимальной, средней и максимальной зарплаты в нём")
+for department in departments:
+    department_salary = []
+    for employer in department['employers']:
+        department_salary.append(employer['salary_rub'])
+    print(f"The minimum salary in department {department['title']} is {min(department_salary)}, average salary is {int(sum(department_salary) / len(department_salary))} and maximum is {max(department_salary)}")
+
+print("\n9. Вывести среднюю зарплату по всей компании")
+for department in departments:
+    for employer in department['employers']:
+        department_salary.append(employer['salary_rub'])
+print(f"The average salary in company {int(sum(department_salary) / len(department_salary))}")
+
+print("\n10. Вывести названия должностей, которые получают больше 90к без повторений")
+positions = set()
+for department in departments:
+    for employer in department['employers']:
+        if employer['salary_rub'] > 90000:
+            positions.add(employer['position'])
+print(f"{', '.join(positions)} earns more than 90,000")
+        
+print("\n11. Посчитать среднюю зарплату по каждому отделу среди девушек (их зовут Мишель, Николь, Кристина и Кейтлин)")
+female_salary = []
+for department in departments:
+    for employer in department['employers']:
+        if employer["first_name"] in ['Michelle', 'Nicole', 'Christina', 'Caitlin']:
+            female_salary.append(employer['salary_rub'])
+    print(f"In the {department['title']} department the average salary for girls is {sum(female_salary)/ len(female_salary)}")
+
+print("\n12. Вывести без повторений имена людей, чьи фамилии заканчиваются на гласную букву")
+last_names = set()
+for department in departments:
+    for employer in department['employers']:
+        print(employer["last_name"][-1])
+        if employer["last_name"][-1] in 'aeijoquy':
+            last_names.add(employer['first_name'])
+print(f"{', '.join(last_names)}")
+
+print("\n13. Вывести список отделов со средним налогом на сотрудников этого отдела")
+
+# 14. Вывести список всех сотредников с указанием зарплаты "на руки" и зарплаты с учётом налогов.
+# 15. Вывести список отделов, отсортированный по месячной налоговой нагрузки.
+# 16. Вывести всех сотрудников, за которых компания платит больше 100к налогов в год.
+# 17. Вывести имя и фамилию сотрудника, за которого компания платит меньше всего налогов.
